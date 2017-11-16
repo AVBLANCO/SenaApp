e31262fe4185156d277e3e6f5e33ad1095ad7936
SENA <sena@CUCDFPSCDLF053.local>
2017-11-15 19:47:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sena/senagram/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.sena.senagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+  public void  crearCuenta(View view){
+      Intent intent =new Intent(this,CreateAccountActivity.class);
+      startActivity(intent);
+
+  }
+}
